b0f46e7ebd1ad71c5f9766d97f92d904bf31cc4d
Geetanjali31 <anjali.mohite31@gmail.com>
2018-07-12 17:19:11 +0530

    Create TestFileFromSubFolder.swift

---
+++ LibraryProj/TestFileFromSubFolder.swift
@@ -0,0 +1,15 @@
+//
+//  TestFileFromSubFolder.swift
+//  LibraryProj
+//
+//  Created by RAC IT SOLUTIONS PVT.LTD on 12/07/18.
+//  Copyright © 2018 Appino. All rights reserved.
+//
+import UIKit
+
+public class TestFileFromSubFolder: NSObject {
+    public func restFileFromSubFolderFunction() {
+        print("\n\nThis is function from cocoapod file's subfolder.........")
+    }
+
+}
